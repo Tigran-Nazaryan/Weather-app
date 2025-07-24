8077d21706da1bdc7299e55677ae7437711be487
Tigran Nazaryan <tigran.nazaryan.18@gmail.com>
2025-07-24 18:36:10 +0400

    add search trigger on Enter key press in SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,17 +1,25 @@
-// SearchBar.jsx
+
 export default function SearchBar({ searchQuery, onChange, onSearch }) {
+   const handleKeyDown = (e) => {
+       if (e.key === "Enter") {
+           e.preventDefault();
+           onSearch(searchQuery);
+       }
+   }
     return (
         <div className="flex w-full max-w-md">
             <input
                 type="text"
-                className="border p-2 w-full"
+                className="border p-2 border-r-0"
                 value={searchQuery}
                 onChange={onChange}
                 placeholder="Enter city"
+                onKeyDown={handleKeyDown}
             />
             <button
                 onClick={onSearch}
-                className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition cursor-pointer"
+                type="submit"
+                className="bg-blue-500 text-white px-4 py-2 hover:bg-blue-600 transition cursor-pointer"
             >
                 Search
             </button>
